7a499e76ab4a993e1311b32442340e4cbfe8d4dd
corepruebas <masuncion.garcia@gmail.com>
2021-06-15 12:54:25 +0200

    m4

--- proyecto/src/main/java/com/pruebas/MyResource.java
+++ proyecto/src/main/java/com/pruebas/MyResource.java
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Bienvenido al curso de...";
+        return "Bienvenido al curso de bots";
     }
 }
